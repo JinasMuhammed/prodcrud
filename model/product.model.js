5b04333cc9644c18328421cd60fefd8f09a016ad
Muhammed Jinas <57747927+JinasMuhammed@users.noreply.github.com>
2020-01-06 12:11:29 +0530

    category changed to gst

--- model/product.model.js
+++ model/product.model.js
@@ -11,7 +11,7 @@ let Product = new Schema(
     title: { type: String },
     image: { type: String },
     price: { type: Number },
-    category: { type: String },
+    gst: { type: Number },
     description: { type: String }
   },
   {
